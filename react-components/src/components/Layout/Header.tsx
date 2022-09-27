e65c18a054cd4da88cd91a300bf8bfc461f247d3
Aleksei P <civil_a@list.ru>
2022-09-27 17:00:32 +0300

    feat: add header style

--- react-components/src/components/Layout/Header.tsx
+++ react-components/src/components/Layout/Header.tsx
@@ -1,53 +1,54 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { NavLink } from 'react-router-dom';
+import styled from 'styled-components';
 import './header.css';
 
-interface NavData {
-  isActive: boolean;
-  isPending: boolean;
-}
+const Header = () => {
+  return (
+    <HeaderContainer>
+      <NavbarContainer>
+        <NaigaitionItem to="/" end>
+          Home
+        </NaigaitionItem>
+        <NaigaitionItem to="/about">About</NaigaitionItem>
+      </NavbarContainer>
+    </HeaderContainer>
+  );
+};
 
-// function Header() {
-//   return (
-//     <header>
-//       <nav>
-//         <NavLink to="/" end className={`${(navData: NavData) => isActiveLink(navData)} button`}>
-//           Home
-//         </NavLink>
-//         <NavLink to="/about" className={`${(navData: NavData) => isActiveLink(navData)} button`}>
-//           About
-//         </NavLink>
-//       </nav>
-//     </header>
-//   );
-// }
+export default Header;
 
-// export default Header;
+const HeaderContainer = styled.header`
+  display: flex;
+  justify-content: center;
+  background-color: var(--header-background);
+  height: 10vh;
+`;
 
-export default class Header extends Component {
-  isActiveLink = (navData: NavData) => {
-    return navData.isActive ? 'active' : 'link';
-  };
+const NavbarContainer = styled.nav`
+  display: flex;
+  align-items: center;
+  gap: 50px;
+`;
 
-  render() {
-    return (
-      <header>
-        <nav>
-          <NavLink
-            to="/"
-            end
-            className={`${(navData: NavData) => this.isActiveLink(navData)} button`}
-          >
-            Home
-          </NavLink>
-          <NavLink
-            to="/about"
-            className={`${(navData: NavData) => this.isActiveLink(navData)} button`}
-          >
-            About
-          </NavLink>
-        </nav>
-      </header>
-    );
+const NaigaitionItem = styled(NavLink)`
+  display: block;
+  height: fit-content;
+  padding: 5px 25px;
+  background-color: var(--link-background);
+  border: 1px solid var (--primary-light);
+  border-radius: 5px;
+  transition all .2s;
+
+	&:hover {
+		color: var(--active-nav); 
   }
-}
+
+  &.active {
+		width: 200%;
+		color: var(--active-nav);
+    border-color: var(--active-nav);
+    box-shadow: 0 0 50px var(--active-nav);
+		letter-spacing: 2px;
+  }
+`;
