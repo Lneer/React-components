000413527b19c4d16d7f96e1bce8e7daa2d08fe5
Aleksei P <civil_a@list.ru>
2022-10-04 01:11:25 +0300

    refactor:notfound  page style

--- react-components/src/pages/NotFound.tsx
+++ react-components/src/pages/NotFound.tsx
@@ -2,6 +2,6 @@ import React, { Component } from 'react';
 
 export default class NotFoundPage extends Component {
   render() {
-    return <div>Page not found</div>;
+    return <h1>Page not found</h1>;
   }
 }
