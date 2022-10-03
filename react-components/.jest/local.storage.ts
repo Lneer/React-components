d323ed7c4df48c0a0688d6fe018c2ab5a2e5bbcf
Aleksei P <civil_a@list.ru>
2022-10-03 18:55:47 +0300

    feat: add seach component tests

---
+++ react-components/.jest/local.storage.ts
@@ -0,0 +1,26 @@
+export function storageMock() {
+	let storage: { [key: string]: string } = {};
+	return {
+		setItem: function (key: string, value?: string) {
+			storage[key] = value || '';
+		},
+
+		getItem: function (key: string) {
+			return key in storage ? storage[key] : null;
+		},
+		removeItem: function (key: string) {
+			delete storage[key];
+		},
+		get length() {
+			return Object.keys(storage).length;
+		},
+		key: function (i: number) {
+			const keys = Object.keys(storage);
+			return keys[i] || null;
+		},
+		clear: function () {
+			storage = {};
+		}
+	};
+}
+window.localStorage = storageMock();
--- react-components/src/__tests__/Search.test.tsx
+++ react-components/src/__tests__/Search.test.tsx
@@ -1,12 +1,50 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import { Search } from '../components';
+import { storageMock } from '../../.jest/local.storage';
+import userEvent from '@testing-library/user-event';
+
+window.localStorage = storageMock();
+const onChange = jest.fn();
+
+beforeEach(() => {
+  window.localStorage.clear();
+});
 
 describe('Search tests', () => {
   it('is render', () => {
-    const func = jest.fn;
-    render(<Search onSearch={func} />);
+    render(<Search onSearch={onChange} />);
 
     expect(screen.getByRole('textbox')).toBeInTheDocument();
+  });
+
+  it('props function call', () => {
+    render(<Search onSearch={onChange} />);
+
+    userEvent.type(screen.getByRole('textbox'), 'test');
+    expect(onChange).toHaveBeenCalledTimes(4);
+  });
+
+  it('get value from localStorage ', () => {
+    window.localStorage.setItem('searchValue', 'testValue');
+    render(<Search onSearch={onChange} />);
+    // expect(screen.debug());
+    expect(screen.queryByDisplayValue('testValue')).toBeInTheDocument();
+  });
+
+  it('put value to the localStorage after unmount component', () => {
+    const { unmount } = render(<Search onSearch={onChange} />);
+
+    userEvent.type(screen.getByRole('textbox'), 'test');
+    unmount();
+    expect(window.localStorage.getItem('searchValue')).toEqual('test');
+  });
+
+  it('update value to the localStorage after update component', () => {
+    render(<Search onSearch={onChange} />);
+
+    userEvent.type(screen.getByRole('textbox'), 'test');
+
+    expect(window.localStorage.getItem('searchValue')).toEqual('test');
   });
 });
