a3e71d29e3c015cdf72d1776c40468400223c979
Aleksei P <civil_a@list.ru>
2022-10-04 00:45:09 +0300

    fix: delete card component from about page

--- react-components/src/pages/About.tsx
+++ react-components/src/pages/About.tsx
@@ -9,8 +9,7 @@ export default class AboutPage extends Component {
         <div>
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim, pariatur. Amet non
           assumenda, aliquid maiores nam numquam est tempora nostrum fugiat vel quia laboriosam
-          dolorum iure dignissimos voluptatem? Aspernatur, ea?
-          <Card></Card>
+          dolorum iure dignissimos voluptatem? Aspernatur,
         </div>
       </>
     );
