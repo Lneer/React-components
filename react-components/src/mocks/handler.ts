df8927dd8775b18cc9e1d04a6eb2ca15224323b9
Aleksei P <civil_a@list.ru>
2022-10-21 00:05:58 +0300

    fix:prettier fix

--- react-components/src/mocks/handler.ts
+++ react-components/src/mocks/handler.ts
@@ -1,19 +1,13 @@
-import { rest } from 'msw'
-import pokemon from '../types/api/1.json'
-import resorceList from '../types/api/2.json'
+import { rest } from 'msw';
+import pokemon from '../types/api/resultList.json';
+import resorceList from '../types/api/persanalInfo.json';
 
 export const handler = [
-	rest.get('https://pokeapi.co/api/v2/pokemon', (req, res, ctx) => {
-		return res(
-			ctx.status(200),
-			ctx.json(resorceList),
-		)
-	}),
+  rest.get('https://pokeapi.co/api/v2/pokemon', (req, res, ctx) => {
+    return res(ctx.status(200), ctx.json(resorceList));
+  }),
 
-	rest.get('https://pokeapi.co/api/v2/pokemon/2/', (req, res, ctx) => {
-		return res(
-			ctx.status(200),
-			ctx.json(pokemon)
-		)
-	})
-]
+  rest.get('https://pokeapi.co/api/v2/pokemon/2/', (req, res, ctx) => {
+    return res(ctx.status(200), ctx.json(pokemon));
+  }),
+];
